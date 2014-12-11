769db5fdeac85700f2388b0f27a8998fe7a5f3ce
Ben Lowry <benlowry@outlook.com>
2014-12-11 12:00:50 -0600

    Improved exiting

--- src/puzzleboss/ExitApp.hx
+++ src/puzzleboss/ExitApp.hx
@@ -31,31 +31,21 @@ import openfl.utils.JNI;
 
 class ExitApp {
 
-	private static var _active:Bool = false;
-
 	public static function enable():Void {
-		_active = true;
-		Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, _onBack);
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, onBack);
 	}
 
 	public static function disable():Void {
-		_active = false;
-		Lib.current.stage.removeEventListener(KeyboardEvent.KEY_UP, _onBack);
+		Lib.current.stage.removeEventListener(KeyboardEvent.KEY_UP, onBack);
 	}
 
 	public static function exit() {
-
-		if(!_active) {
-			return;
-		}
-
 		var sig = "com/puzzleboss/core/ExitApp";
 		var exit = JNI.createStaticMethod(sig, "exitApp", "()V");
 		exit();
 	}
 
-	private static function _onBack(e:KeyboardEvent)
-	{
+	public static function onBack(e:KeyboardEvent) {
 		if(e.keyCode != KeyCodes.BACK) {
 			return;
 		}
